4456cc4dce7aa7592bd044dbc025b7087e029b8d
Tevi Placca <teviplacca@gmail.com>
2024-12-27 16:47:41 -0500

    modify assignment.js file

--- js/assignment.js
+++ js/assignment.js
@@ -1,67 +1,74 @@
 "use strict";
 
-const todos = [
-    "Wash the dishes",
-    "Clean the gutters",
-    "Mow the yard",
+const todos = [ 
+    "Wash the dishes", 
+    "Clean the gutters", 
+    "Mow the yard", 
     "Do some coding",
-    "Rake the leaves",
-];
-
+    "Rake the leaves", 
+]; 
+    
 // Declare your two immutable variables
-
+ const shiftValueEl = document.getElementById("shift-value"); 
+ const popValueEl = document.getElementById("pop-value"); 
+ 
+// Create mutable variables
+ let shiftValue;
+ let popValue; 
 
 // Declaring constant variables to store references to the elements
-// that will be updated by your code.
+
 const answerOneEl = document.getElementById("answer-one");
-const answerTwoEl = document.getElementById("answer-two");
-const answerThreeEl = document.getElementById("answer-three");
-const answerFourEl = document.getElementById("answer-four");
+const answerTwoEl = document.getElementById("answer-two"); 
+const answerThreeEl = document.getElementById("answer-three"); 
+const answerFourEl = document.getElementById("answer-four"); 
 const answerFiveEl = document.getElementById("answer-five");
-const answerSixEl = document.getElementById("answer-six");
-const submissionBtn = document.getElementById("submission-btn");
+const answerSixEl = document.getElementById("answer-six"); 
+const submissionBtn = document.getElementById("submission-btn"); 
 
-function updateAnswerOne() {
-    
+function updateAnswerOne() { 
+    answerOneEl.innerText = todos.join(", ");
+} 
+ 
+ function updateAnswerTwo() { 
+    answerTwoEl.innerText = todos.length;
+} 
+
+ function removeLastValue() { 
+    popValue = todos.pop(); 
+    answerThreeEl.innerText = todos.join(", "); 
+    popValueEl.innerText = popValue;
+} 
+
+ function removeFirstValue() { 
+    shiftValue = todos.shift(); 
+    answerFourEl.innerText = todos.join(", "); 
+    shiftValueEl.innerText = shiftValue; 
+} 
+
+function addShiftAndPopValues() { 
+    if (shiftValue) { 
+        todos.push(shiftValue);
+     } 
+     if (popValue) { 
+        todos.unshift(popValue); 
+    } answerFiveEl.innerText = todos.join(", "); 
+} 
+
+function reverseTodoList() { 
+    todos.reverse(); 
+    answerSixEl.innerText = todos.join(", "); 
 }
 
-function updateAnswerTwo() {
-
+function render() { 
+    updateAnswerOne();
+    updateAnswerTwo();
+    removeLastValue();
+    removeFirstValue();
+    addShiftAndPopValues();
+    reverseTodoList();
 }
 
-function removeLastValue() {
-
-}
-
-function removeFirstValue() {
-
-}
-
-function addShiftAndPopValues() {
-
-}
-
-function updateAnswerFour() {
-
-}
-
-function reverseTodoList() {
-
-}
-
-function updateAnswerFive() {
-
-}
-
-function updateAnswerSix() {
-
-}
-
-function render() {
-
-}
-
-submissionBtn.addEventListener("click", function () {
-    // Update this function
-    render();
+submissionBtn.addEventListener("click", function () { 
+    render()
 });
